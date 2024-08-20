30df03e73877f262aa6788b9168686da93eedada
nask08 <nask88@gmail.com>
2024-08-20 11:06:55 +0530

    Pushed CatalogAPI changes

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+        }
+    }
+}
